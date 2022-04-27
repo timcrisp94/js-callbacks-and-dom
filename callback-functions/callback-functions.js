7c639f4e5bdba1615ab18513a8d1eac1bd4aefaa
Tim Crisp <timothycrisp@gmail.com>
2022-04-27 13:08:19 -0500

    init commit

---
+++ callback-functions/callback-functions.js
@@ -0,0 +1,22 @@
+console.log('hi')
+
+
+function main() {
+  borderline()
+  welcome()
+  greeting()
+}
+
+console.log(main())
+
+function borderline() {
+  console.log('---------')
+}
+
+function welcome() {
+  console.log('HELLO')
+}
+
+function greeting(){
+  console.log('and how are you today?')
+} 
