7f642b6fb0d01bf35f1f96b9dae70b51fdfc1f71
Tim Crisp <timothycrisp@gmail.com>
2022-04-27 13:44:06 -0500

    use filter method to solve for wellDrivenCars

--- callback-functions/callback-functions.js
+++ callback-functions/callback-functions.js
@@ -1,22 +1,29 @@
 console.log('hi')
 
+const cars = [
+  { make: 'Toyota', yrsOld: 5, mileage: 92399 },
+  { make: 'Ford', yrsOld: 12, mileage: 255005 },
+  { make: 'Ferrari', yrsOld: 9, mileage: 12966 },
+  { make: 'Subaru', yrsOld: 9, mileage: 111266 },
+  { make: 'Toyota', yrsOld: 2, mileage: 41888 },
+  { make: 'Tesla', yrsOld: 3, mileage: 57720 }
+]
 
-function main() {
-  borderline()
-  welcome()
-  greeting()
-}
+// use the filter method to select the objects within cars that have been driven more than 20,000 miles per year
 
-console.log(main())
+console.log(cars)
 
-function borderline() {
-  console.log('---------')
-}
+let wellDrivenCars = cars.filter(function(car) {
+  // console.log(car.mileage)
+  let milesPerYear = car.mileage / car.yrsOld
+  if (milesPerYear > 20000){
+    return car
+  }
+})
 
-function welcome() {
-  console.log('HELLO')
-}
+console.log(wellDrivenCars)
 
-function greeting(){
-  console.log('and how are you today?')
-} 
+let wellDrivennCars = cars.filter(car => (car.mileage / car.yrsOld) > 20000)
+
+console.log(wellDrivennCars)
+
