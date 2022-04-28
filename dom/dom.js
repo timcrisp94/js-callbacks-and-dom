6b54ea8889d472d64e99e413f450ac0e6b19df28
Tim Crisp <timothycrisp@gmail.com>
2022-04-28 17:21:39 -0500

    work with modifying elements

--- dom/dom.js
+++ dom/dom.js
@@ -1,4 +1,8 @@
 console.log('dom')
+const titleElement = document.querySelector('.title')
+const pElement = document.querySelector('.cool')
+pElement.innerText = 'something new!'
+titleElement.innerText = 'the dom dom bay bay'
 
-const titleElement = document.getElementById('main-title')
-console.log(titleElement)
+textContent = document.getElementById('text-content')
+textContent.textContent = 'textContent - Used to retrieve/set content as plain text'
