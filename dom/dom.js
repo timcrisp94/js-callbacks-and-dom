dc26c56c1a1e497ac6f9d6e818ed48ea3172806e
Tim Crisp <timothycrisp@gmail.com>
2022-04-28 17:35:49 -0500

    addd random image using set attribute

--- dom/dom.js
+++ dom/dom.js
@@ -6,3 +6,19 @@ titleElement.innerText = 'the dom dom bay bay'
 
 textContent = document.getElementById('text-content')
 textContent.textContent = 'textContent - Used to retrieve/set content as plain text'
+
+titleElement.style.textAlign = 'center'
+
+pElement.style.color = 'purple'
+pElement.style.backgroundColor = 'pink'
+
+img = document.getElementById('img')
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+img.setAttribute(
+  "src",
+  `https://picsum.photos/${getRandomIntInclusive(200, 800)}`
+)
