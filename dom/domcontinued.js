01f06ad3cd164ec0c8248b3414602221546f6fa5
Tim Crisp <timothycrisp@gmail.com>
2022-04-29 09:29:00 -0500

    manipulate multiple elements using qsa

--- dom/domcontinued.js
+++ dom/domcontinued.js
@@ -6,3 +6,14 @@ htmlCollection.setAttribute(
   'href',
   'https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection'
 )
+
+commentElements = document.querySelectorAll('.comment')
+console.log(commentElements)
+
+commentElements[0].textContent = 'yay'
+
+for (let commentElement of commentElements) {
+  commentElement.style.fontSize = "30px"
+  commentElement.style.color = "orange"
+  commentElement.style.backgroundColor = "brown"
+}
